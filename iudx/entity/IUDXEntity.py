b41cb8ad0c8ffe26dee44885ad2b788f3a215c7c
blurred-machine <paras.varshney97@gmail.com>
2021-01-21 11:03:07 +0530

    added stub code for IUDXEntity

---
+++ iudx/entity/IUDXEntity.py
@@ -0,0 +1,51 @@
+"""Module doc string. Leave empty for now.
+
+IUDXEntity.py
+"""
+from typing import TypeVar, Generic, Any, List, Dict
+
+from iudx.cat.Catalogue import Catalogue
+from iudx.cat.CatalogueQuery import CatalogueQuery
+from iudx.rs.ResourceServer import ResourceServer
+
+from iudx.entity.Entity import Entity
+from iudx.entity.Entities import Entities
+
+IUDXEntity = TypeVar('T')
+
+
+class IUDXEntity():
+    """Abstract class for IUDXEntity. Helps to create a modular interface
+       for the IUDX Entity.
+    """
+
+    def __init__(self):
+        """Pydoc heading.
+        Args:
+            argument (argument-type): argument-description
+        Returns:
+            returned-varaible (returned-varaible-type): return-variable-description
+        """
+        self.catalogue: Catalogue = None
+        self._rs: ResourceServer = None
+        self.resources: List[Dict] = None
+        self.resourceGroups: Dict = None
+        return
+
+    def get_entites(self, query: CatalogueQuery=None) -> Entities:
+        """Pydoc heading.
+        Args:
+            argument (argument-type): argument-description
+        Returns:
+            returned-varaible (returned-varaible-type): return-variable-description
+        """
+        return self
+
+    def get_entity(self, entity_id: str=None) -> Entity:
+        """Pydoc heading.
+        Args:
+            argument (argument-type): argument-description
+        Returns:
+            returned-varaible (returned-varaible-type): return-variable-description
+        """
+        return self
