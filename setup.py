74062dcd948136ccdee4b897208c74f603b9f67e
blurred-machine <paras.varshney97@gmail.com>
2021-03-03 14:49:34 +0530

    Implemented CLI interface for downloading entity's data and print latest entity data on console. Updated the Readme file with IPython sample examples and cli examples.

--- setup.py
+++ setup.py
@@ -4,7 +4,12 @@ setup(
     name='iudx',
     version='2.0.1',
     packages=find_packages(),
-    install_requires=['requests', 'urllib3'],
+    install_requires=['requests', 'urllib3', 'pandas', 'click'],
+    entry_points={
+        "console_scripts": [
+            "iudx=iudx.entity.Entity:Entity.cli",
+        ],
+    },
     url='https://github.com/datakaveri/iudx-python-sdk',
     license='GNU General Public License v3.0',
     author='Paras V, Sreekrishnan, Rakshit R',
