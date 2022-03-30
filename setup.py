82c04ef5cc78338f43d7d34fd17d860bb39a5865
rraks <rakshitadmar@gmail.com>
2022-03-30 17:58:41 +0530

    [Update] Requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='iudx',
     version='2.0.1',
     packages=find_packages(),
-    install_requires=['requests', 'urllib3', 'pandas', 'click'],
+    install_requires=['requests', 'urllib3', 'pandas', 'click', 'tqdm'],
     entry_points={
         "console_scripts": [
             "iudx=iudx.entity.Entity:Entity.cli",
@@ -12,7 +12,7 @@ setup(
     },
     url='https://github.com/datakaveri/iudx-python-sdk',
     license='GNU General Public License v3.0',
-    author='Paras V, Sreekrishnan, Rakshit R',
+    author='Paras V, Sahil Mishra, Dhruthick Gowda, Rakshit R',
     author_email='rakshit.rameshd@datakaveri.org',
     description='Python SDK for IUDX.'
     )
